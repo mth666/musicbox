c484e230976899aa2f7cf9c32b9f294158118c39
Myo Than Htike <85684641+mth666@users.noreply.github.com>
2024-08-15 16:50:25 +0700

    added seven buttons and it's sound notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+//import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +13,109 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Music Box',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('assets/note1.wav'),
+                  );
+                },
+                child: Text('Note1'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('assets/note2.wav'),
+                  );
+                },
+                child: Text('Note2'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('assets/note3.wav'),
+                  );
+                },
+                child: Text('Note3'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('assets/note4.wav'),
+                  );
+                },
+                child: Text('Note4'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('assets/note5.wav'),
+                  );
+                },
+                child: Text('Note5'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('assets/note6.wav'),
+                  );
+                },
+                child: Text('Note6'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(
+                    AssetSource('assets/note7.wav'),
+                  );
+                },
+                child: Text('Note7'),
+              ),
+            ),
+          ],
+        ),
       ),
-      //home: const musicBox(title: 'Flutter Demo Home Page'),
     );
   }
 }
+/*      child: TextButton(
+            onPressed: () async {
+              AudioCache.instance = AudioCache(prefix: '');
+              final player = AudioPlayer();
+              await player.play(
+                AssetSource('assets/note1.wav'),
+              );
+            },
+            child: const Text('click meee'),
+          ),
+
+ */
